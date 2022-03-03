d4c2e7e1affddc669125f19d639b32f3411d7986
Jose Wong <josemawo17@gmail.com>
2022-03-03 16:04:33 -0500

    spring-boot-first-config

---
+++ src/main/java/com/avatar/startwars/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.avatar.startwars;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StartWarsApplication.class);
+	}
+
+}
